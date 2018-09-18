e047add1e55adeb0c3b64188207df0cf18d3d1c8
Gilbert Torchon <gilbert@noukod.com>
2018-09-18 13:17:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(katz_deli)
+   if katz_deli.size == 0
+      puts "The line is currently empty."
+   else
+     
+      katz_deli.each_with_index do |member,index|
+        puts "#{member}"
+      end
+   end
+end
